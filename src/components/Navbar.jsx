f50a5352a9d4702923486502870fd42047b9fdf5
JhimmieC137 <toluwalope.david@gmail.com>
2026-05-13 22:55:52 +0100

    Changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink, Link } from 'react-router-dom';
-import logo from '../assets/logo.svg';
+import logo from '../assets/PCS_logo.png';
 
 const navLinks = [
   // { to: '/', label: 'Home' },
